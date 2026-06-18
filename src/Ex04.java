c9ed65bdd670f386c2a68789bb8fb1d36d60842c
a1l1ke <onetenthplease@gmail.com>
2026-06-18 11:18:06 +0900

    feat: 정방향 배열 출력 구현

--- src/Ex04.java
+++ src/Ex04.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Ex04 {
+    public static void main(String[] args) {
+        // 배열 + for => for Each
+        String[] arr = {"apple", "banana", "cherry"};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("i = " + i);
+            System.out.println("arr[" + i + "] = " + arr[i]);
+        }
+    }
 }
