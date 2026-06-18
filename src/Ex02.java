3e93ba5b7979e10900fc616a814a75f4b41a9176
a1l1ke <onetenthplease@gmail.com>
2026-06-18 10:34:15 +0900

    feat: 정방향 while 루프 구현

--- src/Ex02.java
+++ src/Ex02.java
@@ -2,9 +2,14 @@ public class Ex02 {
     public static void main(String[] args) {
         // for + while
         // fori
+//        int i = 0; // scope 문제
         // 정방향 (++)
         for (int i = 1; i <= 9; i++) {
             System.out.println("i = " + i);
+        }
+        int w = 1;
+        while (w <= 9) {
+            System.out.println("w = " + w++);
         }
     }
 }
