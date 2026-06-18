5e378e3ed5c856df61c02a973fa0702f79197c9e
a1l1ke <onetenthplease@gmail.com>
2026-06-18 11:30:18 +0900

    feat: 이중 boolean 배열 선언 및 deepToString 출력 추가

--- src/Ex04.java
+++ src/Ex04.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Ex04 {
     public static void main(String[] args) {
         // 배열 + for => for Each
@@ -13,9 +15,13 @@ public class Ex04 {
             System.out.println("idx = " + idx);
             System.out.println("arr[idx] = " + arr[idx]);
         }
-        for (int i = arr.length - 1; i >= 0; i--) {
-            System.out.println("i = " + i);
-            System.out.println("arr[" + i + "] = " + arr[i]);
-        }
+        // 이중포문 -> 이중배열
+        boolean[][] matrix = {
+                // j 0 1 2 3 4
+                {true, false, false, false, false}, // i = 0
+                {true, true, true, true, true}, // i = 1
+                {false, false, false, false, true} // // i = 2
+        };
+        System.out.println("Arrays.deepToString(matrix) = " + Arrays.deepToString(matrix));
     }
 }
