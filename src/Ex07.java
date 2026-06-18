016cc04267d88551301836a6c63857e803017fb4
a1l1ke <onetenthplease@gmail.com>
2026-06-18 12:47:27 +0900

    feat: Map 생성 및 데이터 추가, 조회, 수정 구현

--- src/Ex07.java
+++ src/Ex07.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Ex07 {
+    public static void main(String[] args) {
+        // K, V -> 키 타입, 값 타입
+        Map<String, Integer> map = new HashMap<>();
+        map.put("apple", 1000);
+        map.put("banana", 2000); // key -> value
+        System.out.println("map = " + map);
+        System.out.println("map.get(\"apple\") = " + map.get("apple"));
+//        map.put("banana", 20000);
+        map.put("banana", map.get("banana") * 10);
+        System.out.println("map = " + map);
+    }
 }
