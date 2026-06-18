917ff98beb2d6f44e2c673522ffbf89947d0664f
a1l1ke <onetenthplease@gmail.com>
2026-06-18 10:34:29 +0900

    feat: 삼중 for문 구현

--- src/Ex02.java
+++ src/Ex02.java
@@ -35,5 +35,16 @@ public class Ex02 {
                 // 맨 안에 있는 루프가 가장 먼저 종료되고 나서야 다음 루프의 다음 단계로 진행한다
             }
         }
+        // 삼중 for문 (금단의 다중포문) - 실무적으로는 이런 일이 많이 없고, 알고리즘적으로는 복잡도 최악
+        for (int i = 0; i <= 9; i++) {
+            for (int j = 0; j <= 9; j++) {
+                for (int k = 0; k <= 9; k++) {
+                    System.out.println("i = " + i);
+                    System.out.println("j = " + j);
+                    System.out.println("k = " + k);
+                    System.out.println(i + " * " + j + " * " + k + " = " + i * j * k);
+                }
+            }
+        }
     }
 }
