aa6f2a6292133bdb32b0f856dc3a4968c6842551
a1l1ke <onetenthplease@gmail.com>
2026-06-18 10:17:41 +0900

    feat: 정방향 for 루프 구현

--- src/Ex02.java
+++ src/Ex02.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Ex02 {
+    public static void main(String[] args) {
+        // for + while
+        // fori
+        // 정방향 (++)
+        for (int i = 1; i <= 9; i++) {
+            System.out.println("i = " + i);
+        }
+    }
 }
