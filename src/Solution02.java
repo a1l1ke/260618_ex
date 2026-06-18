b38ceb6571a267c067f0fa9ab97686ef0fc441a9
a1l1ke <onetenthplease@gmail.com>
2026-06-18 15:38:39 +0900

    feat: Solution02 폰켓몬 문제 기본 구조 및 개수 파악 기능 구현

---
+++ src/Solution02.java
@@ -0,0 +1,14 @@
+public class Solution02 {
+    public int solution(int[] nums) {
+        // System.out.println(Arrays.toString(nums));
+        // N마리의 폰켓몬이 있는데, 이중에 최대 N/2마리가 최대.
+        int N = nums.length;
+        // int max = N / 2;
+        // System.out.println("N : " + N + " max : " + max);
+        // int answer = 0;
+        // int answer = max;
+        // 가장 많은 종류의 폰켓몬을 선택하는 방법
+        
+        return 0;
+    }
+}
