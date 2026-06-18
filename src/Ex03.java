215a08eae488a8177c3c89d55d64f4574898062a
a1l1ke <onetenthplease@gmail.com>
2026-06-18 10:39:35 +0900

    feat: 기본 변수 선언 및 출력 추가

--- src/Ex03.java
+++ src/Ex03.java
@@ -1,6 +1,9 @@
 public class Ex03 {
     public static void main(String[] args) {
         // array & for & for-each
-
+        int i1 = 1;
+        int i2 = 2;
+        int i3 = 3;
+        System.out.println(i1 + " " + i2 + " " + i3);
     }
 }
