e4026ca61cdc98a04dabb0b6edcf99574e569ae4
a1l1ke <onetenthplease@gmail.com>
2026-06-18 10:34:19 +0900

    feat: 역방향 루프 구현

--- src/Ex02.java
+++ src/Ex02.java
@@ -11,5 +11,17 @@ public class Ex02 {
         while (w <= 9) {
             System.out.println("w = " + w++);
         }
+
+        // 역방향 (--)
+//        for (int i = 1; i <= 9; i--) { // 무한 반복
+        for (int i = 9; i > 0; i--) { // 무한 반복
+            // 종료조건을 못 만나서
+            System.out.println("i = " + i);
+        }
+        w = 9;
+        while (w >= 1) {
+            System.out.println("w = " + w--);
+        }
+        // scope.
     }
 }
