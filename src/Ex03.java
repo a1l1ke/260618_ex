11640630d32734c1bd19e332adade77f8824886b
a1l1ke <onetenthplease@gmail.com>
2026-06-18 10:39:40 +0900

    feat: 배열 초기화 및 재할당 예제 추가

--- src/Ex03.java
+++ src/Ex03.java
@@ -5,5 +5,13 @@ public class Ex03 {
         int i2 = 2;
         int i3 = 3;
         System.out.println(i1 + " " + i2 + " " + i3);
+//        int[] arr1;
+//        arr1 = new int[3]; // 선언과 할당이 나눠져 있음
+        int[] arr1 = new int[3]; // 배열 초기화;
+        // 할당이 되면 더 이상 길이를 바꿀 수 X. (그 할당된 배열은.)
+        // 새롭게 다른 아예 새로운 배열을 할당함으로 대체할 순 있음.
+        System.out.println("arr1 = " + arr1);
+        arr1 = new int[5];
+        System.out.println("arr1 = " + arr1); // immutable -> 대체
     }
 }
